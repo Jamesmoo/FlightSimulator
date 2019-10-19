d30e002efe645c855d104a30ae9e3e1d7e0678e2
James Adams <Jamesmoo2090@gmail.com>
2019-10-19 07:42:54 -0600

    cleanup
    cleanup

--- FlightSim/FlightSim/wwwroot/js/scoreboard.js
+++ FlightSim/FlightSim/wwwroot/js/scoreboard.js
@@ -1,59 +1,55 @@
 $(function(){
-  function getLeaderboard(){
-    $.ajax({
-        url: "/scoreboardlisting",
-        type : 'GET',
-        dataType : 'json',
-        success : function(data) {              
-            loadResults(data);
-        },
-        error : function(request, error){
-            console.error("Could not Retrieve Leaderboard scores");
-        }
-    });
-  }
-    
-  function loadResults(data){
-    var count = 1;
-    for(const record of data){
-      var htmlRecord = "";
-
-      if(record.rank === 1){
-        htmlRecord = "<tr class='record'><td class='winner num-"+ count +"'>" 
-          + "<img src='./images/star.png' class='star-align'>"
-          + record.rank 
-          + "</td><td>" 
-          + record.user + "</td><td>" 
-          + record.score + "</td><td>" 
-          + record.time + "</td></tr>";
-      
-      }else{
-        htmlRecord = "<tr class='record'><td class='num-"+ count +"'>" 
-          + record.rank + "</td><td>" 
-          + record.user + "</td><td>" 
-          + record.score + "</td><td>" 
-          + record.time + "</td></tr>";
-      }
-
-      $(htmlRecord).appendTo('tbody').hide().show(2000);  
-      count++;
+    function getLeaderboard(){
+        $.ajax({
+            url: "/scoreboardlisting",
+            type : 'GET',
+            dataType : 'json',
+            success : function(data) {              
+                loadResults(data);
+            },
+            error : function(request, error){
+                console.error("Could not Retrieve Leaderboard scores");
+            }
+        });
     }
+    
+    function loadResults(data){
+        var count = 1;
+        for(const record of data){
+            var htmlRecord = "";
+            if(record.rank === 1){
+                htmlRecord = "<tr class='record'><td class='winner num-" + count + "'>"
+                    + "<img src='./images/star.png' class='star-align'>"
+                    + record.rank + "</td><td>";
+            } else {
+                htmlRecord = "<tr class='record'><td class='num-" + count + "'>"
+                    + record.rank + "</td><td>"; 
+            }
 
-      $('.record').mouseenter(function () {
-        $(this).addClass('record-focus');
-    });
+            htmlRecord += record.user + "</td><td>"
+                + record.score + "</td><td>"
+                + record.time + "</td></tr>";
+                
+            $(htmlRecord).appendTo('tbody').hide().show(2000);  
+            count++;
+        }
 
-    $('.record').mouseleave(function () {
-        $(this).removeClass('record-focus');
-    });
+        $('.record').mouseenter(function () {
+            //$(this).addClass('record-focus');
+        });
 
-      $('.record').on('click', function () {
-          var user = $(this).find('td')[1].innerText.replace(" ", "_");
-          window.location.href = "/track.html?user=" + user;
-      });
-  }
+        $('.record').mouseleave(function () {
+            //$(this).removeClass('record-focus');
+        });
+
+        $('.record').on('click', function () {
+            //var user = encodeURIComponent($(this).find('td')[1].innerText);
+            //window.location.href = "/track.html?user=" + user;
+            $(this)
+        });
+    }
   
-  $(window).bind("load", function(){
-    getLeaderboard();
-  });
+    $(window).bind("load", function(){
+        getLeaderboard();
+    });
 });
